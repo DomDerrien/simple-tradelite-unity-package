7560cf94c037bd52e896820947e0e1953b54cd3b
DomDerrien <dominique.derrien@gmail.com>
2022-01-17 14:52:51 -0500

    Tests for BaseError

---
+++ Tests/Runtime/Model/BaseErrorTest.cs
@@ -0,0 +1,30 @@
+using System;
+using NUnit.Framework;
+using Tradelite.SDK.Model;
+
+namespace Tradelite.SDK.Model.Test {
+
+    public class BaseErrorTest {
+        [Test]
+        public void TestFreshBaseErrorToString() {
+            string message = "attr1";
+            BaseError entity =  new BaseError(message);
+            Assert.AreEqual(message, entity.message);
+            Assert.AreEqual("message: `attr1`", entity.ToString());
+        }
+
+        [Test]
+        public void TestCompleteBaseErrorToString() {
+            string message = "attr1";
+            Exception cause = new NotImplementedException("attr2");
+            BaseError entity =  new BaseError(message, cause);
+            Assert.AreEqual(message, entity.message);
+            Assert.AreEqual(cause, entity.cause);
+            Assert.AreEqual(
+                "message: `attr1`, " + 
+                "cause: `System.NotImplementedException: attr2`", 
+                entity.ToString()
+            );
+        }
+    }
+}
